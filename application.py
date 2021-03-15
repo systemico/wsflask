4833218ed31fd1d1d86605e47b2b8dd69330572b
Edwin Ariza <me@edwinariza.com>
2021-03-15 10:15:45 -0500

    Incluyendo el SSL y el CORS para el WS.

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from module.main import module
+from flask_sslify import SSLify
+from flask_cors import CORS
 from config.startup import Startup
 import logging
 # Rut apara la carga de archivos.
@@ -11,6 +13,8 @@ logging.basicConfig(filename='/var/tmp/wsflask.log', level=logging.DEBUG)
 
 # Cargamos la aplicación Flask.
 application = Flask(__name__)
+sslify = SSLify(application)
+cors = CORS(application, resources={r"/*": {"origins": "*"}})
 # Registramos el module.
 application.register_blueprint(module, url_prefix='/module')
 
