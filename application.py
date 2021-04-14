c0f7d9bb9f660b9dabd1772f43485bcdbef9bd23
Edwin Ariza <me@edwinariza.com>
2021-04-14 12:22:33 -0500

    MODFUN: Including error catch and 404 error management.

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from module.main import module
 from flask_sslify import SSLify
 from flask_cors import CORS
@@ -18,14 +18,36 @@ cors = CORS(application, resources={r"/*": {"origins": "*"}})
 # Registramos el module.
 application.register_blueprint(module, url_prefix='/module')
 
-'''
-ENDPOINT de inicio de la application.
-'''
 @application.route('/')
 def iniciar():
-    # Crear las variables de sesion que requiera el proyecto.
-    return 'Bienvenidos al proyecto!'
+    '''
+    Permite manera la carga de la primera version de la aplciación.
+    :return:
+    '''
+    return render_template('bienvenida.html')
 
+
+@application.errorhandler(404)
+def page_not_found(e):
+  '''
+  Permite el manejo detallado de errores 404 en toda la aplicación.
+  :param e:
+  :return:
+  '''
+  logging.warning(e)
+  return render_template('404.html'), 404
+
+@application.errorhandler(500)
+def server_error(e):
+  '''
+  Permite el de los errores que hagan fallar el servidor.
+  :param e:
+  :return:
+  '''
+  logging.error(e)
+  return render_template('500.html'), 500
+
+# SE EJECUTA CUANDO SE LLAMA ESTE ARCHIVO COMO PRINCIPAL
 if __name__ == '__main__':
     application.debug = True
     application.run(host='0.0.0.0')
